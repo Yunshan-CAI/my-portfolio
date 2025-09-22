99f579df0797e582bea7ebd8173d528c34494ece
Yunshan-CAI <caiyunshan321@gmail.com>
2025-09-22 16:51:44 +0200

    some small changes

--- data/headerNavLinks.ts
+++ data/headerNavLinks.ts
@@ -1,7 +1,7 @@
 const headerNavLinks = [
   { href: '/', title: 'Home' },
   { href: '/blog', title: 'Blog' },
-  { href: '/projects', title: 'Projects' },
+  { href: '/projects', title: 'Creative Projects' },
   { href: '/about', title: 'About' },
 ]
 
